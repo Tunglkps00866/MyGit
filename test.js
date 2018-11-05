eac4e606302a403014a8b3f59bec4db20e3a2cfc
Tunglkps00866 <tunglkps00866@gmail.com>
2018-11-05 21:19:44 +0700

    Update 5/11

--- test.js
+++ test.js
@@ -1,17 +1,5 @@
-var timeLeft = 30;
-var elem = document.getElementById('some_div');
-var timerId = setInterval(countdown, 1000);
-
-function countdown() {
-    if (timeLeft == -1) {
-        clearTimeout(timerId);
-        doSomething();
-    } else {
-        elem.innerHTML = timeLeft + ' seconds remaining';
-        timeLeft--;
-    }
-}
-
-function doSomething() {
-    alert("Hi");
-}
+          $(function()
+          {
+              $("#sort").sortable();
+              $("#sort").disableselection();
+          });
